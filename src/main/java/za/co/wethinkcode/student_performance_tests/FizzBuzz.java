0dab59ca542cb1099df157e181358f3ffd6196b0
mmoss <mmoss020@student.wethinkcode.co.za>
2022-06-06 10:20:02 +0200

    done

--- src/main/java/za/co/wethinkcode/student_performance_tests/FizzBuzz.java
+++ src/main/java/za/co/wethinkcode/student_performance_tests/FizzBuzz.java
@@ -2,6 +2,23 @@ package za.co.wethinkcode.student_performance_tests;
 
 public class FizzBuzz {
     // TODO implement this class
+    public FizzBuzz() {
+    }
+    public static String process(int i) {
+        boolean divisible_by_5 = i%5==0;
+        boolean divisible_by_3 = i%3==0;
+        String result = "";
+        if (divisible_by_3){
+            result += "Fizz";
+        }
+        if (divisible_by_5){
+            result += "Buzz";
+        }
+        if (!divisible_by_3 && !divisible_by_5){
+            return Integer.toString(i);
+        }
+        return result;
+    }
 
     public static void main(String[] args){
         for (int i = 1; i <= 100; i++){
